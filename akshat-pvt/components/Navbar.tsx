a3c6945f6a52b8bc1c56fc1c717cb28ad68173d0
AkshatJha21 <akshatj2134@gmail.com>
2024-07-31 02:05:12 +0530

    navbar init

--- akshat-pvt/components/Navbar.tsx
+++ akshat-pvt/components/Navbar.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-screen'>
+        <div className='mx-auto w-[80%] mt-4 lg:w-[60%] p-3 bg-zinc-800 rounded-lg'>
+            <div className='flex items-center justify-between px-2'>
+                <p>Logo</p>
+                <p className='text-sm'>Portfolio</p>
+                <p className='text-sm'>Pricing</p>
+                <p className='text-sm'>Contact</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
