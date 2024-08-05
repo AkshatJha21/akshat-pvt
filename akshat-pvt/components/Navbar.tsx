06b345687d89d8ee42ef829dd7af58813d4d7679
AkshatJha21 <akshatj2134@gmail.com>
2024-08-05 10:14:25 +0530

    5/8

--- akshat-pvt/components/Navbar.tsx
+++ akshat-pvt/components/Navbar.tsx
@@ -11,7 +11,8 @@ const Navbar = () => {
     <div className='w-screen'>
         <div className='mx-auto w-[80%] mt-4 lg:w-[60%] p-3 dark:bg-zinc-800/40 border-[0.5px] dark:border-zinc-700/40 rounded-lg'>
             <div className='flex items-center justify-between px-2'>
-                <Image src={logo} alt='Akshat' height={40}/>
+                {/* <Image src={logo} alt='Akshat' height={40}/> */}
+                <h1 className='font-bold text-lg'>2One</h1>
                 <div className='hidden md:flex text-sm justify-between dark:text-white w-3/5'>
                   <p className='border border-dashed border-transparent hover:border-rose-500 rounded-sm px-4 py-2 dark:hover:text-rose-500 cursor-pointer transition-all'>Portfolio</p>
                   <p className='border border-dashed border-transparent hover:border-rose-500 rounded-sm px-4 py-2 dark:hover:text-rose-500 cursor-pointer transition-colors'>Work with me</p>
