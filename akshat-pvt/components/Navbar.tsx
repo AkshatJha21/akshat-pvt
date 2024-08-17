0ce339dbdeb59ddd0f7b3a98c5ba9f5052148b48
AkshatJha21 <akshatj2134@gmail.com>
2024-08-17 13:27:35 +0530

    todo: add button variant

--- akshat-pvt/components/Navbar.tsx
+++ akshat-pvt/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
                 <div className='flex md:hidden items-center'>
                   Menu
                   <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 10 4 4 4-4"/>
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m8 10 4 4 4-4"/>
                   </svg>
                 </div>
                 <ModeToggle />
