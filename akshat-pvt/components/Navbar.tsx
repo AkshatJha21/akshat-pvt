3ac0260f085b806f459571dc6fd9ffd10f77e7db
AkshatJha21 <akshatj2134@gmail.com>
2024-08-17 13:07:18 +0530

    17/08

--- akshat-pvt/components/Navbar.tsx
+++ akshat-pvt/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { ModeToggle } from './ThemeToggle';
 const Navbar = () => {
   return (
     <div className='w-screen'>
-        <div className='mx-auto w-[80%] mt-4 lg:w-[60%] p-3 dark:bg-zinc-800/40 border-[0.5px] dark:border-zinc-700/40 rounded-lg'>
+        <div className='mx-auto w-[80%] mt-4 lg:w-[60%] p-3 dark:bg-zinc-800/40 border-[0.5px] dark:border-zinc-700/40 rounded-xl'>
             <div className='flex items-center justify-between px-2'>
                 <h1 className='font-bold text-lg'>2One</h1>
                 <div className='hidden md:flex text-sm justify-between dark:text-white w-3/5'>
