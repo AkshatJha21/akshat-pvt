cbbbda94519c627e432848eb3c2a163c8b9d54ca
AkshatJha21 <akshatj2134@gmail.com>
2024-08-18 00:10:33 +0530

    18/08

--- akshat-pvt/components/HamburgerMenu.tsx
+++ akshat-pvt/components/HamburgerMenu.tsx
@@ -13,7 +13,7 @@ const HamburgerMenu = () => {
         <DropdownMenu>
             <DropdownMenuTrigger>
                 <Button variant={'dotted'} className='flex items-center'>
-                    Menu
+                    Menu 
                     <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m8 10 4 4 4-4"/>
                     </svg>
