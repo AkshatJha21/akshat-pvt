382add8d51cdcebc89a65bc7603fc07e8656bd12
AkshatJha21 <akshatj2134@gmail.com>
2024-08-17 13:24:12 +0530

    mobile menu init

--- akshat-pvt/components/Navbar.tsx
+++ akshat-pvt/components/Navbar.tsx
@@ -14,6 +14,12 @@ const Navbar = () => {
                   <p className='border border-dashed border-transparent hover:border-rose-500 rounded-sm px-4 py-2 dark:hover:text-rose-500 cursor-pointer transition-colors'>Work with me</p>
                   <p className='border border-dashed border-transparent hover:border-rose-500 rounded-sm px-4 py-2 dark:hover:text-rose-500 cursor-pointer transition-colors'>Connect</p>
                 </div>
+                <div className='flex md:hidden items-center'>
+                  Menu
+                  <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 10 4 4 4-4"/>
+                  </svg>
+                </div>
                 <ModeToggle />
             </div>
         </div>
