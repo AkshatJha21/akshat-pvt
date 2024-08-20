2bfd1356fcea3ecc797a138e84c7822143ff0668
AkshatJha21 <akshatj2134@gmail.com>
2024-08-20 23:18:07 +0530

    distribution update

--- akshat-pvt/app/page.tsx
+++ akshat-pvt/app/page.tsx
@@ -1,9 +1,9 @@
-import MainLogo from "@/components/Home/MainLogo";
+import Landing from "@/components/Home/Landing";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <MainLogo />
+    <div>
+      <Landing />
     </div>
   );
 }
---
+++ akshat-pvt/components/Home/Landing.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from 'react'
+import MainLogo from './MainLogo';
+
+const Landing = () => {
+  return (
+    <div className='h-screen flex flex-col items-center justify-center'>
+        <MainLogo />    
+        {/* it's 21 line */}
+        {/* Short explanation of what we do */}
+        {/* Button: Get down to business */}
+    </div>
+  )
+}
+
+export default Landing
