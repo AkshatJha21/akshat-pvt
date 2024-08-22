a00301346dc37a40fba2fe51018618bc43bc6a89
AkshatJha21 <akshatj2134@gmail.com>
2024-08-22 22:18:43 +0530

    22/08

--- akshat-pvt/components/Home/Landing.tsx
+++ akshat-pvt/components/Home/Landing.tsx
@@ -7,6 +7,7 @@ const Landing = () => {
   return (
     <div className='h-screen flex flex-col items-center justify-center'>
         <MainLogo />    
+        <p>It's 21, in case you're wondering</p>
         {/* it's 21 line */}
         {/* Short explanation of what we do */}
         {/* Button: Get down to business */}
