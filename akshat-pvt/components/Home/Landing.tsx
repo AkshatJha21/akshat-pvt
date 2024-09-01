befefb5b328d7ad6dc95f4e63575c2e4bbb8aa5d
AkshatJha21 <akshatj2134@gmail.com>
2024-09-01 08:58:05 +0530

    01/09

--- akshat-pvt/components/Home/Landing.tsx
+++ akshat-pvt/components/Home/Landing.tsx
@@ -8,7 +8,7 @@ const Landing = () => {
     <div className='h-screen flex flex-col items-center justify-center'>
         <MainLogo />    
         {/* After logo animation, the pronunctiation should fade in (the below p tag) */}
-        {/* <p>It's 21 </p> */} 
+        {/* <p className='mt-8'> (It's 21) </p>  */}
         {/* it's 21 line */}
         {/* Short explanation of what we do */}
         {/* Button: Get down to business */}
