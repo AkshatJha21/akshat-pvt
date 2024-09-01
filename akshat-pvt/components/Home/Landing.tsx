e9d5cf6071cbb049e64189293ed3be9eeaedce7a
AkshatJha21 <akshatj2134@gmail.com>
2024-09-01 11:51:05 +0530

    copy added

--- akshat-pvt/components/Home/Landing.tsx
+++ akshat-pvt/components/Home/Landing.tsx
@@ -1,17 +1,27 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import MainLogo from './MainLogo';
+import DottedButton from '../DottedButton';
+import { motion } from 'framer-motion';
 
 const Landing = () => {
+  const str = "21 Devs: Your all-in-one digital solution, delivering top-notch design and development — fast and on time.";
+  
+
   return (
     <div className='h-screen flex flex-col items-center justify-center'>
-        <MainLogo />    
-        {/* After logo animation, the pronunctiation should fade in (the below p tag) */}
-        {/* <p className='mt-8'> (It's 21) </p>  */}
-        {/* it's 21 line */}
-        {/* Short explanation of what we do */}
-        {/* Button: Get down to business */}
+        <MainLogo />  
+          {/* Subheading div below */}
+        
+          <div
+            className='py-4 md:py-8 flex md:flex-row gap-y-4 flex-col md:w-[50%] items-center justify-center'
+          >
+            <p className='md:text-[16px] text-pretty md:text-balance md:w-3/5 w-4/5'>
+              {str}
+            </p>
+            <DottedButton />
+          </div>
     </div>
   )
 }
