bd7e08a37b103891c20ed75eaa1093402b983d8f
AkshatJha21 <akshatj2134@gmail.com>
2024-09-02 15:33:34 +0530

    Landing Cleanup

--- akshat-pvt/components/Home/Landing.tsx
+++ akshat-pvt/components/Home/Landing.tsx
@@ -2,12 +2,8 @@
 
 import React, { useState } from 'react'
 import MainLogo from './MainLogo';
-import DottedButton from '../DottedButton';
-import { motion } from 'framer-motion';
 
 const Landing = () => {
-  const str = "21 Devs: Your all-in-one digital solution, delivering top-notch design and development — fast and on time.";
-  
 
   return (
     <div className='h-screen flex flex-col items-center justify-center'>
